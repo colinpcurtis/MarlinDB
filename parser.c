c63c78f66a3bab16465b8e0b5c3b65c75485a741
Colin Curtis <colinpcurtis826@ucla.edu>
2022-04-03 21:33:04 -0700

    add json support
    change up server and cli to support json, also update README
    and python client

--- parser.c
+++ parser.c
@@ -1,26 +1,34 @@
 #include <string.h>
-#include "parser.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <json-c/json.h>
 
-Data split_input(char* line, char** split_line) {
-    char* token = strtok(line, " ");
-    int splits = 0;
-    while (token != NULL) {
-        token[strcspn(token, "\n")] = 0;
-        split_line[splits] = token;
-        token = strtok(NULL, " ");
-        splits++;
-    }
-    split_line[splits] = NULL;
+#include "parser.h"
+#include "globals.h"
+
+Data split_input(char* line) {
+    struct json_object *parsed_json;
+    struct json_object *operation;
+	struct json_object *key;
+	struct json_object *value;
+
+    parsed_json = json_tokener_parse(line);
+
+	json_object_object_get_ex(parsed_json, "operation", &operation);
+	json_object_object_get_ex(parsed_json, "key", &key);
+	json_object_object_get_ex(parsed_json, "value", &value);
+
+    printf("Operation: %s\n", json_object_get_string(operation));
+	printf("Key: %s\n", json_object_get_string(key));
+	printf("Value: %s\n", json_object_get_string(value));
+
     Data data;
 
-    if (splits >= 1) {
-        strcpy(data.operation, split_line[0]);
-    }
-    if (splits >= 2) {
-        strcpy(data.key, split_line[1]);
-    }
-    if (splits >= 3) {
-        strcpy(data.value, split_line[2]);
+    strcpy(data.operation, json_object_get_string(operation));
+    strcpy(data.key, json_object_get_string(key));
+    if (json_object_get_string(value) != NULL) {
+        strcpy(data.value, json_object_get_string(value));
     }
 
     return data;
